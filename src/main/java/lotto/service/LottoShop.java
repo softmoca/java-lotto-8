33636cfeccc4e8df71cc63e843174d61c67f5d44
softmoca <acyc0909@gmail.com>
2025-10-29 18:26:37 +0900

    feat : 6개의 랜덤한 숫자로 생성된 로또 구현

--- src/main/java/lotto/service/LottoShop.java
+++ src/main/java/lotto/service/LottoShop.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
 
@@ -10,11 +12,22 @@ public class LottoShop {
 
     public List<Lotto> buyLottos(int purchaseAmount) {
         int quantity = calculateQuantity(purchaseAmount);
-
+        return generateLottos(quantity);
     }
 
     private int calculateQuantity(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    private List<Lotto> generateLottos(int quantity) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
     }
 
 
