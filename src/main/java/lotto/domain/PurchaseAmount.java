380ccfc6f6f323f84a228e958ecbead42cbffbd8
softmoca <acyc0909@gmail.com>
2025-10-30 17:27:26 +0900

    refactor : 로또 구매비용 도메인에 Enum 에러 메시지 반영

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
-import lotto.exception.ErrorMessage;
+import static lotto.exception.ErrorMessage.INVALID_PURCHASE_AMOUNT_FORMAT;
+import static lotto.exception.ErrorMessage.INVALID_PURCHASE_AMOUNT_INSUFFICIENT;
+import static lotto.exception.ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT;
 
 public class PurchaseAmount {
 
@@ -23,7 +25,7 @@ public class PurchaseAmount {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("구입 금액은 숫자로 입력해야 합니다.");
+            throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_FORMAT.getMessage());
         }
     }
 
@@ -35,17 +37,13 @@ public class PurchaseAmount {
 
     private void validatePositive(int amount) {
         if (amount < MINIMUM_AMOUNT) {
-            throw new IllegalArgumentException(
-                    ErrorMessage.INVALID_PURCHASE_AMOUNT_POSITIVE.getMessage()
-            );
+            throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_INSUFFICIENT.getMessage());
         }
     }
 
     private void validateUnit(int amount) {
         if (amount % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(
-                    "구입 금액은 " + LOTTO_PRICE + "원 단위여야 합니다."
-            );
+            throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_UNIT.getMessage());
         }
     }
 
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,7 +4,7 @@ package lotto.exception;
 public enum ErrorMessage {
     // 구입 금액 관련
     INVALID_PURCHASE_AMOUNT_FORMAT("구입 금액은 숫자여야 합니다."),
-    INVALID_PURCHASE_AMOUNT_POSITIVE("구입 금액은 0보다 커야 합니다."),
+    INVALID_PURCHASE_AMOUNT_INSUFFICIENT("구입 금액은 1,000원 보다 커야 합니다."),
     INVALID_PURCHASE_AMOUNT_UNIT("구입 금액은 1,000원 단위여야 합니다.");
 
 
