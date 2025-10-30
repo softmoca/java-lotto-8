8488b6cac5bccc82b99fe8f08494374ffd3ecaf1
softmoca <acyc0909@gmail.com>
2025-10-30 21:22:22 +0900

    feat : INVALID_NUMBER_FORMAT 숫자형식 아닌 경우 생성되는 에러 메시지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,7 +2,7 @@ package lotto.exception;
 
 
 public enum ErrorMessage {
-    // 구입 금액 관련
+    INVALID_NUMBER_FORMAT("숫자 형식이 올바르지 않습니다."),
     INVALID_PURCHASE_AMOUNT_FORMAT("구입 금액은 숫자여야 합니다."),
     INVALID_PURCHASE_AMOUNT_INSUFFICIENT("구입 금액은 1,000원 보다 커야 합니다."),
     INVALID_PURCHASE_AMOUNT_UNIT("구입 금액은 1,000원 단위여야 합니다."),
--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -1,5 +1,7 @@
 package lotto.util;
 
+import static lotto.exception.ErrorMessage.INVALID_NUMBER_FORMAT;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,7 +20,7 @@ public class InputParser {
         try {
             return Integer.parseInt(input.trim());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자 형식이 올바르지 않습니다.");
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT.getMessage());
         }
     }
 
