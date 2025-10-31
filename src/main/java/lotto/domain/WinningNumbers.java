55b58ea10e32838d31e063f61782934d1ecb7bd4
softmoca <acyc0909@gmail.com>
2025-10-31 10:53:43 +0900

    feast  : WinningNumbers 도메인 추가
    - 당첨 번호(Lotto)와 보너스 번호(BonusNumber)를 하나로 묶은 개념
    - 구매한 로또와의 당첨 확인 책임

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+
+public class WinningNumbers {
+    private final Lotto winningLotto;
+    private final BonusNumber bonusNumber;
+
+    public WinningNumbers(Lotto winningLotto, BonusNumber bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Rank match(Lotto purchasedLotto) {
+        int matchCount = countMatches(purchasedLotto);
+        boolean matchesBonus = purchasedLotto.contains(bonusNumber.getValue());
+        return Rank.valueOf(matchCount, matchesBonus);
+    }
+
+
+    private int countMatches(Lotto purchasedLotto) {
+        return (int) purchasedLotto.getNumbers().stream()
+                .filter(winningLotto::contains)
+                .count();
+    }
+}
