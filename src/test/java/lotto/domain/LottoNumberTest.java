dd8ad8c5b13ca2164c25150f1a301b2abdf88265
softmoca <acyc0909@gmail.com>
2025-11-13 22:40:20 +0900

    test(LottoNumber) : 동등성 테스트 추가
    - Lotto 안에서 중복 체크
    - Lotto가 보너스 번호 포함

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -32,6 +33,22 @@ class LottoNumberTest {
 
         assertThatCode(() -> new LottoNumber(20))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 같은_번호는_동등하다() {
+        LottoNumber number1 = new LottoNumber(1);
+        LottoNumber number2 = new LottoNumber(1);
+
+        assertThat(number1).isEqualTo(number2);
+    }
+
+    @Test
+    void 다른_번호는_동등하지_않다() {
+        LottoNumber number1 = new LottoNumber(1);
+        LottoNumber number2 = new LottoNumber(2);
+
+        assertThat(number1).isNotEqualTo(number2);
     }
 
 }
