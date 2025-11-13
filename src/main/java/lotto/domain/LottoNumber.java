1bb08e3b9c87646f7ebb33d1b6ef4f46cc34cef8
softmoca <acyc0909@gmail.com>
2025-11-13 22:36:59 +0900

    feat(LottoNumbers) : 46이상 생성 예외 검증 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,10 +3,11 @@ package lotto.domain;
 public class LottoNumber {
 
     public LottoNumber(int number) {
-        if (number == 0) {
+        if (number < 1 || number > 45) {
             throw new IllegalArgumentException(
                     "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
             );
         }
     }
 }
+
