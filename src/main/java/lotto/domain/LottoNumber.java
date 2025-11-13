d949e31da3c5166ee9eb750c508e4975da9209ab
softmoca <acyc0909@gmail.com>
2025-11-13 22:35:27 +0900

    feat(LottoNumbers) : 생성 최소 구현 및 예외 검증 구현

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class LottoNumber {
+
+    public LottoNumber(int number) {
+        if (number == 0) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+            );
+        }
+    }
+}
