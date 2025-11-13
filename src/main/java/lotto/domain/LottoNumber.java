6d162df2f5548eb099ec9792c1b1f30009a6623d
softmoca <acyc0909@gmail.com>
2025-11-13 22:38:21 +0900

    refactor(LottoNumbers) : 상수 및 메서드 추출

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,9 +1,18 @@
 package lotto.domain;
 
 public class LottoNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private final int number;
 
     public LottoNumber(int number) {
-        if (number < 1 || number > 45) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(
                     "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
             );
