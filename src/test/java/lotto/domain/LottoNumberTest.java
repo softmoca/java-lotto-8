bb5cb54c3322b0c83248cb14db5d9c8453ffd748
softmoca <acyc0909@gmail.com>
2025-11-13 22:33:57 +0900

    test(LottoNumber) : 로또 번호 생성 예외 메시지 테스트 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -9,6 +9,7 @@ class LottoNumberTest {
     @Test
     void 로또_번호는_1보다_작을_수_없다() {
         assertThatThrownBy(() -> new LottoNumber(0))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1부터 45");
     }
 }
