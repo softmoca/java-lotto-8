166e088dbcd5ce2e2a4234a49ca174f0e2b20ea5
softmoca <acyc0909@gmail.com>
2025-11-14 14:35:05 +0900

    feat(LottoStaticstics) : 통계 생성 및 당첨 결과 추가 기능 구현

---
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoStatistics {
+    private final int purchaseAmount;
+    private final Map<Rank, Integer> statistics;
+
+    public LottoStatistics(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+        this.statistics = new HashMap<>();
+    }
+
+    public void add(Rank rank) {
+        statistics.merge(rank, 1, Integer::sum);
+    }
+
+    public int getCount(Rank rank) {
+        return statistics.getOrDefault(rank, 0);
+    }
+}
