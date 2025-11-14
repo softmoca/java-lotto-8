a0d11d2de51bdf32a780ff103a11402cbbd75f4e
softmoca <acyc0909@gmail.com>
2025-11-14 13:06:06 +0900

    test(Rank) : 2등과 3등 테스트 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -12,4 +12,20 @@ class RankTest {
 
         assertThat(rank).isEqualTo(Rank.FIRST);
     }
+
+    @Test
+    void _5개_일치하고_보너스_일치하면_2등이다() {
+        Rank rank = Rank.of(5, true);
+
+        assertThat(rank).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    void _5개_일치하고_보너스_불일치하면_3등이다() {
+        Rank rank = Rank.of(5, false);
+
+        assertThat(rank).isEqualTo(Rank.THIRD);
+    }
+
+
 }
