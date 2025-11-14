20fc79f25ac4ba949678922ae23fa36b6faed7f7
softmoca <acyc0909@gmail.com>
2025-11-14 14:00:40 +0900

    feat(Rank): 등수 판별 낙첨 등급 기능 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,14 +1,14 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 public enum Rank {
     FIRST(6, 2_000_000_000, false),
     SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false);
+    FIFTH(3, 5_000, false),
+    NONE(0, 0, false);
 
     private final int matchCount;
     private final int prizeMoney;
@@ -24,13 +24,18 @@ public enum Rank {
         return prizeMoney;
     }
 
-    public static Optional<Rank> of(int matchCount, boolean hasBonus) {
+    public static Rank of(int matchCount, boolean hasBonus) {
         return Arrays.stream(values())
                 .filter(rank -> rank.matches(matchCount, hasBonus))
-                .findFirst();
+                .findFirst()
+                .orElse(NONE);
     }
 
     private boolean matches(int matchCount, boolean hasBonus) {
+        if (this == NONE) {
+            return false;
+        }
+
         if (this.matchCount != matchCount) {
             return false;
         }
