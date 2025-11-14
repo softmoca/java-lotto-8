c5db0478cfe8b46cbbefbe1a9230a2cf816ee963
softmoca <acyc0909@gmail.com>
2025-11-14 17:06:23 +0900

    feat(Money) : 구입 금액 생성 및 검증 기능 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+public class Money {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_AMOUNT = 1;
+
+    private final int amount;
+
+    public Money(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount < MIN_AMOUNT) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 구입 금액은 양수여야 합니다."
+            );
+        }
+
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 구입 금액은 1,000원 단위여야 합니다."
+            );
+        }
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+}
