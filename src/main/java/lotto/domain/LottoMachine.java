a444f99bd56a8df4e9e5922d0e48b546bce1908c
softmoca <acyc0909@gmail.com>
2025-11-14 14:57:43 +0900

    feat(LottoMachine) : 랜덤 번호로 로또 발행 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,13 +1,24 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
 
     public LottoMachine() {
     }
 
-    public void issue(int purchaseAmount) {
+    public List<Lotto> issue(int purchaseAmount) {
         validatePurchaseAmount(purchaseAmount);
+
+        int count = purchaseAmount / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(createLotto());
+        }
+        return lottos;
     }
 
     private void validatePurchaseAmount(int purchaseAmount) {
@@ -17,4 +28,11 @@ public class LottoMachine {
             );
         }
     }
+
+    private Lotto createLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+
 }
