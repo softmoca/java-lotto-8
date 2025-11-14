5f54ddfaacee7f0918065e0f8a5f9ed04c22a48c
softmoca <acyc0909@gmail.com>
2025-11-14 12:44:15 +0900

    feat(Lotto) : 로또 번호 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -26,6 +27,16 @@ public class Lotto {
             throw new IllegalArgumentException(
                     "[ERROR] 로또 번호는 중복될 수 없습니다."
             );
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(
+                        "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+                );
+            }
         }
     }
 
