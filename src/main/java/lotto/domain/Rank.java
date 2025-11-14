c3afe42763667e553397012224a5355b603ed601
softmoca <acyc0909@gmail.com>
2025-11-14 13:06:59 +0900

    feat(Rank) : 1등~3등 기능 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,9 +1,22 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST;
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH;
 
     public static Rank of(int matchCount, boolean hasBonus) {
+        if (matchCount == 6) {
+            return FIRST;
+        }
+        if (matchCount == 5 && hasBonus) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
         return FIRST;
     }
 }
