9f3064a3ae3cdfe840e1497920dcbb0c6ef50890
softmoca <acyc0909@gmail.com>
2025-11-14 13:17:37 +0900

    feat(Rank): 등수별 상금 보유 기능 구현

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,21 @@ package lotto.domain;
 import java.util.Optional;
 
 public enum Rank {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH;
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000);
+
+    private final int prizeMoney;
+
+    Rank(int prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
 
     public static Optional<Rank> of(int matchCount, boolean hasBonus) {
         if (matchCount == 6) {
