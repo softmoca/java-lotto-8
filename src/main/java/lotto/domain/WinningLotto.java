52b59ad2a2cf9bceec6a33710c94c358ae2cfdfd
softmoca <acyc0909@gmail.com>
2025-11-14 14:21:05 +0900

    feat(WinningLotto) : 당첨 번호 보너스 번호 중복 검증 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -10,6 +10,14 @@ public class WinningLotto {
     public WinningLotto(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = new Lotto(winningNumbers);
         this.bonusNumber = new LottoNumber(bonusNumber);
+        validateBonusNotDuplicate();
     }
 
+    private void validateBonusNotDuplicate() {
+        if (winningNumbers.containsBonus(bonusNumber.getNumber())) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
+            );
+        }
+    }
 }
