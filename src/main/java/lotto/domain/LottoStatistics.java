3f4c37f71700b6b098d79b5b84605466f5cb876e
softmoca <acyc0909@gmail.com>
2025-11-17 13:02:01 +0900

    feat(LottoStatistics) : 통계 수집 책임추가

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,24 +1,43 @@
 package lotto.domain;
 
-
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LottoStatistics {
     private final int purchaseAmount;
     private final Map<Rank, Integer> statistics;
 
-    public LottoStatistics(int purchaseAmount) {
+    private LottoStatistics(int purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
         this.statistics = new HashMap<>();
     }
 
-    public void add(Rank rank) {
+    public static LottoStatistics from(List<Lotto> lottos,
+                                       WinningLotto winningLotto,
+                                       int purchaseAmount) {
+        LottoStatistics statistics = new LottoStatistics(purchaseAmount);
+
+        for (Lotto lotto : lottos) {
+            Rank rank = winningLotto.match(lotto);
+            statistics.add(rank);
+        }
+
+        return statistics;
+    }
+
+    private void add(Rank rank) {
         statistics.put(rank, statistics.getOrDefault(rank, 0) + 1);
     }
 
     public int getCount(Rank rank) {
         return statistics.getOrDefault(rank, 0);
+    }
+
+    public List<RankStatistic> getRankStatistics() {
+        return Rank.getWinningRanksInDisplayOrder().stream()
+                .map(rank -> RankStatistic.of(rank, getCount(rank)))
+                .toList();
     }
 
     public int getTotalPrize() {
@@ -30,8 +49,6 @@ public class LottoStatistics {
 
     public double getProfitRate() {
         double rate = (double) getTotalPrize() / purchaseAmount * 100;
-
         return Math.round(rate * 10) / 10.0;
     }
-
 }
