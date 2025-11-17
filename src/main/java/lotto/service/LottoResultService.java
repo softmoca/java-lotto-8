d1090f9d5d5953e578ec159bbe75c892c88984dc
softmoca <acyc0909@gmail.com>
2025-11-17 14:25:20 +0900

    feat(LottoResultService) : 통계 결과 생성 및 반환 기능 구현

---
+++ src/main/java/lotto/service/LottoResultService.java
@@ -0,0 +1,20 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoStatistics;
+import lotto.domain.ProfitRate;
+import lotto.domain.RankStatistic;
+
+public class LottoResultService {
+
+    public LottoResult createResult(LottoStatistics statistics, int purchaseAmount) {
+        List<RankStatistic> rankStats = statistics.getRankStatistics();
+        ProfitRate profitRate = ProfitRate.calculate(
+                statistics.getTotalPrize(),
+                purchaseAmount
+        );
+
+        return LottoResult.of(rankStats, profitRate.getValue());
+    }
+}
