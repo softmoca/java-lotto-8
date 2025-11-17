37460c5aaa21358b994b9fdaf3efcf3630c58f81
softmoca <acyc0909@gmail.com>
2025-11-17 14:59:37 +0900

    test(LottoMachine) : static 변환 테스트

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -11,9 +10,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 class LottoMachineTest {
 
     @Test
-    void 로또_발행기를_생성한다() {
-        assertThatCode(() -> new LottoMachine())
-                .doesNotThrowAnyException();
+    void 로또를_발행한다() {
+        // given
+        Money money = Money.from(3000);
+
+        // when
+        List<Lotto> lottos = LottoMachine.issue(money);
+
+        // then
+        assertThat(lottos).hasSize(3);
+        assertThat(lottos.get(0).getNumbers()).hasSize(6);
     }
 
     @ParameterizedTest(name = "{0}원 -> {1}장")
@@ -24,13 +30,13 @@ class LottoMachineTest {
             "10000, 10"
     })
     void 다양한_구입_금액으로_로또를_발행한다(int amount, int expectedCount) {
-        LottoMachine machine = new LottoMachine();
+        // given
         Money money = Money.from(amount);
 
-        List<Lotto> lottos = machine.issue(money);
+        // when
+        List<Lotto> lottos = LottoMachine.issue(money);
 
+        // then
         assertThat(lottos).hasSize(expectedCount);
     }
-
-
 }
