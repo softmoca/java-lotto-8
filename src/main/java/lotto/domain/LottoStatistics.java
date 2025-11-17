376fcd87e568f6ec81d915f2c0334e74694b9970
softmoca <acyc0909@gmail.com>
2025-11-17 14:19:58 +0900

    refactor(LottoStatistic) : 수익률 책임 제거

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -11,7 +11,6 @@ public class LottoStatistics {
         this.statistics = statistics;
     }
 
-
     public static LottoStatistics from(List<Lotto> lottos, WinningLotto winningLotto) {
         Map<Rank, Integer> stats = new HashMap<>();
 
@@ -23,28 +22,19 @@ public class LottoStatistics {
         return new LottoStatistics(stats);
     }
 
-    public LottoResult createResult(int purchaseAmount) {
-        List<RankStatistic> rankStats = createRankStatistics();
-        double profitRate = calculateProfitRate(purchaseAmount);
-        return LottoResult.of(rankStats, profitRate);
-    }
-
-    private List<RankStatistic> createRankStatistics() {
+    public List<RankStatistic> getRankStatistics() {
         return Rank.getWinningRanksInDisplayOrder().stream()
                 .map(rank -> RankStatistic.of(rank, getCount(rank)))
                 .toList();
     }
 
-    private int getCount(Rank rank) {
-        return statistics.getOrDefault(rank, 0);
-    }
-
-    private double calculateProfitRate(int purchaseAmount) {
-        int totalPrize = statistics.entrySet().stream()
+    public int getTotalPrize() {
+        return statistics.entrySet().stream()
                 .mapToInt(entry -> entry.getKey().getPrizeMoney() * entry.getValue())
                 .sum();
+    }
 
-        double rate = (double) totalPrize / purchaseAmount * 100;
-        return Math.round(rate * 10) / 10.0;
+    private int getCount(Rank rank) {
+        return statistics.getOrDefault(rank, 0);
     }
 }
